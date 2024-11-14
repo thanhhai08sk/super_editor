01516e81c06e06f25a8c5c642a91188897809fa9
hai <thanhhai08sk@gmail.com>
2024-11-14 07:36:30 +0700

    workaround for: Couldn't map an IME position to a document position.

--- super_editor/lib/src/default_editor/document_ime/document_serialization.dart
+++ super_editor/lib/src/default_editor/document_ime/document_serialization.dart
@@ -290,6 +290,14 @@ class DocumentImeSerializer {
       }
     }
 
+    //My workaround: if the IME position is not found in any of the ranges, return the last position.
+    final range = imeRangesToDocTextNodes.keys.last;
+    return DocumentPosition(
+      nodeId: imeRangesToDocTextNodes.values.last,
+      nodePosition: TextNodePosition(offset: imePosition.offset - range.start),
+    );
+
+
     editorImeLog.shout("---------------DocumentImeSerializer----------------------");
     editorImeLog.shout("Couldn't map an IME position to a document position.");
     editorImeLog.shout("Desired IME position: '$imePosition'");
